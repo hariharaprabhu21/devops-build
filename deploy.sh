9c6cfec7bf3fcc9f3752bda1fd527641100f7293
EC2 Default User <ec2-user@ip-172-31-82-149.ec2.internal>
2024-12-06 18:57:50 +0000

    Add debug output to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Enable debugging to see each command executed
+set -x
+
 echo "Starting deployment..."
 
 # Get the current branch name
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "Deploying branch: $BRANCH_NAME"
 
 # Check the branch and deploy accordingly
 if [ "$BRANCH_NAME" == "dev" ]; then
@@ -38,5 +42,6 @@ else
     echo "Unsupported branch: $BRANCH_NAME. Exiting."
     exit 1
 fi
+
 
 
